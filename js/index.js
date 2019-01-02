c5c07fb745515a106eb0508a7bf9368233095016
rkosh <rajatkosh@insynchq.com>
2019-01-03 00:20:28 +0530

    Update footer links and description

--- js/index.js
+++ js/index.js
@@ -3,29 +3,29 @@ $(document).ready(function(){
 	var desc = $('#desc');
 	var img = $('#image');
 
-	$('li').on('mouseover', function(){
-		image = $(this).attr('image')
-		text = $(this).attr('desc')
+	$('li, a').on('mouseover', function(){
+			image = $(this).attr('image')
+			text = $(this).attr('desc')
 
-		if (!text) {
-			clear();
-		} else {
-			img.css({
-				'background' : 'url('+image+') no-repeat center',
-				'background-size': 'contain'
-			});
-			desc.html('<p>" '+text+' "</p>');
-		}
-	});
+			if (!text) {
+				clear();
+			} else {
+				img.css({
+					'background' : 'url('+image+') no-repeat center',
+					'background-size': 'contain'
+				});
+				desc.html('<p>" '+text+' "</p>');
+			}
+		});
 
-	$('li').on('mouseout', clear);
+	$('li, a').on('mouseout', clear);
 
 	function clear() {
 		img.css({
 				'background' : 'url() no-repeat center',
 				'background-size': 'contain'
 			});
-		desc.html('<p></p>');	
+		desc.html('<p></p>');
 	}
 
 })
